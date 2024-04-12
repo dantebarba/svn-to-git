3c0298fb1901c6d5719ee44382d097c53b295a3b
Dante Barba <dantebarba@gmail.com>
2024-04-12 15:31:19 -0300

    Update push-to-remote.sh

--- push-to-remote.sh
+++ push-to-remote.sh
@@ -5,10 +5,13 @@ RED='\033[0;31m';
 LIGHT_GREEN='\e[1;32m';
 NC='\033[0m' # No Color
 
+ABSOLUTE_PATH="${ABSOLUTE_PATH:-$(pwd)}"
 ###########################
 ####### GIT 
 # Git repository to migrate
 GIT_URL="${GIT_URL:-https://git.mycompany.com/git/repo/sistemas/$PROJECT_NAME.git}"
+
+cp .gitignore $ABSOLUTE_PATH/$PROJECT_NAME
 
 echo
 echo -e "${LIGHT_GREEN} [RUN] Step 07/08 ${NC}"
